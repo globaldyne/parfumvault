bd3eafd6bbc861d663e2ddc01c3d15fadfef5f34
John <hey@globaldyne.co.uk>
2020-05-15 09:05:18 +0000

    config update

--- inc/config.php
+++ inc/config.php
@@ -3,6 +3,6 @@
 $dbhost = 'localhost'; //MySQL Hostname
 $dbuser = 'pvault'; //MySQL Username
 $dbpass = 'pvault'; //MySQL Password
-$dbname = 'pvault'; //MySQL DB name
+$dbname = 'vault'; //MySQL DB name
 
 ?>
