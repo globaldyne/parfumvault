cf556b79678cb530c22fb966bf26c1887077caff
John <hey@globaldyne.co.uk>
2020-06-27 09:38:14 +0000

    Pro version branding

--- pages/about.php
+++ pages/about.php
@@ -8,29 +8,14 @@
     <td>&nbsp;</td>
   </tr>
   <tr>
-    <td><p>A simple tool to help perfumers organize their formulas and ingredients.</p>
-    <p>This is a FREE software provided as is without ANY waranty under MIT <a href="LICENSE.txt" target="_blank">license</a>.</p>
-    <pre><p>The MIT License (MIT)</p><p>Copyright (c) 2017-2020
-</p><p>Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the &quot;Software&quot;), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-<p>The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.</p>
-<p>THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.</p></pre>
+    <td><p>A sophisticated tool to help perfumers organize their formulas, ingredients and inventory.
+    </p>
+      <pre>&nbsp;</pre>
 </td>
     <td>&nbsp;</td>
   </tr>
   <tr>
-    <td><p>Version: <strong><?php echo file_get_contents('../VERSION.md');?></strong></p>
+    <td><p>Version: <strong><?php echo file_get_contents('../VERSION.md');?></strong> PRO</p>
     <p><a href="https://github.com/globaldyne/parfumvault" target="_blank"><strong>Github Repo</strong></a></p>
     <p>Theme by <a href="https://startbootstrap.com/themes/sb-admin-2/" target="_blank"><strong>SB Admin 2</strong></a></p></td>
     
