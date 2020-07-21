adfa4926e9e337ab64c67c52c357411112048ffc
John <hey@globaldyne.co.uk>
2020-07-21 17:40:34 +0000

    Version check update

--- func/checkVer.php
+++ func/checkVer.php
@@ -2,14 +2,14 @@
 if (!defined('pvault_panel')){ die('Not Found');}
 
 function checkVer($ver) {
-	$gitHubRep = 'https://www.jbparfum.com/';
-	$githubVer = $gitHubRep.'proVERSION.md';
+	$githubVer = 'https://raw.githubusercontent.com/globaldyne/parfumvault/master/VERSION.md';
+	$gitHubRep = 'https://www.jbparfum.com/features/';
 	
 	$data = trim(file_get_contents($githubVer));
 	if($ver < $data){	
 		echo '<div class="alert alert-info alert-dismissible">
                 <a href="#" class="close" data-dismiss="alert" aria-label="close">x</a>
-  		<strong>New <a href="'.$gitHubRep.'pro" target="_blanc">version ('.$data.')</a> is availale!</strong>
+  		<strong>New <a href="'.$gitHubRep.'" target="_blanc">version ('.$data.')</a> is availale!</strong>
 		</div>';
 	}
 
