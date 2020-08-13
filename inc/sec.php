6b5d34ae4b185d62ae79dcb70e4c95a433980494
John <hey@globaldyne.co.uk>
2020-08-13 07:05:35 +0000

    Security update

--- inc/sec.php
+++ inc/sec.php
@@ -4,7 +4,8 @@ define('__ROOT__', dirname(dirname(__FILE__)));
 
 session_start();
 if(!isset($_SESSION['parfumvault'])){
-	header('Location: login.php');
+	$login = $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].'/login.php';
+	header('Location: '.$login);
 	exit;
 }
 
