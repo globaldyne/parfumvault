da6237f39cb5af55f7d17378527f928c20e1464c
John <hey@globaldyne.co.uk>
2020-12-23 14:05:33 +0000

    Code improvements

--- func/getFormula.php
+++ func/getFormula.php
@@ -1,14 +1,12 @@
 <?php if (!defined('pvault_panel')){ die('Not Found');}?>
 
 <?php
-function getFormula($formula,$profile,$dbhost,$dbuser,$dbpass,$dbname){
-	
-	$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die ('Error connecting to database');
+function getFormula($formula,$profile,$conn){
 	$fname = mysqli_real_escape_string($conn, $formula);
 	$ing = mysqli_query($conn, "SELECT ingredient FROM formulas WHERE name = '$fname'");
 	
 	while($q = mysqli_fetch_array($ing)){
-		$chk = mysqli_query($conn, "SELECT name FROM ingredients WHERE name = '$q[ingredient]' AND profile = '$profile'");
+		$chk = mysqli_query($conn, "SELECT name FROM ingredients WHERE name = '".$q['ingredient']."' AND profile = '$profile'");
 		while ($qValues=mysqli_fetch_array($chk)){
 			echo $qValues['name'].'\n';
 		}
