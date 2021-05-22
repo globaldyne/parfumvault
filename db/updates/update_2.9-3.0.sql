7269d8921989105509b9116da4b01aa0add85fb3
John <hey@globaldyne.co.uk>
2021-05-22 10:47:21 +0000

    DB Schema update

--- db/updates/update_2.9-3.0.sql
+++ db/updates/update_2.9-3.0.sql
@@ -11,7 +11,9 @@ CREATE TABLE `suppliers` (
  UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-INSERT INTO suppliers (`ingID`,`ingSupplierID`,`supplierLink`,`size`,`price`,`manufacturer`) SELECT id,'1',supplier_link,ml,price,manufacturer FROM ingredients;
+INSERT INTO suppliers (`ingSupplierID`,`ingID`,`supplierLink`,`price`,`size`,`manufacturer`,`preferred`) SELECT DISTINCT B.id,  A.id, A.supplier_link, A.price, A.ml, A.manufacturer, '1' FROM  ingredients AS A, ingSuppliers AS B WHERE B.name = A.supplier ;
+
+
 ALTER TABLE `ingredients` DROP `supplier`, DROP `supplier_link`, DROP `price`, DROP `ml`, DROP `manufacturer`;
 ALTER TABLE `cart` DROP `supplier`, DROP `supplier_link`; 
 ALTER TABLE `cart` ADD `ingID` INT NOT NULL AFTER `id`; 
