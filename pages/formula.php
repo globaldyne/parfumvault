4407d207cf5a662360ad59ed7a774e531a5bc7e0
JB <john@globaldyne.co.uk>
2022-05-14 10:45:52 +0100

    A major rewrite in formula view to utilize select2 (WIP)

--- pages/formula.php
+++ pages/formula.php
@@ -279,74 +279,14 @@ $base_ex = get_formula_excludes($conn, $fid, 'base');
   </div>
 </div>
 
-<script type="text/javascript" language="javascript" >
-//$(document).ready(function(){
+<script src="/js/select-ingredient.js"></script>
+<script>
 document.title = "<?=$meta['name'].' - '.$product?>";
 
 $("#concentration").prop("disabled", true); 
 $("#dilutant").prop("disabled", true);
 $('#quantity').prop("disabled", true);
 
-let ingredientsLit = $('#ingredient');
-ingredientsLit.empty();
-ingredientsLit.append('<option selected="true" disabled>Choose ingredient</option>');
-ingredientsLit.prop('selectedIndex', 0);
-
-$.ajax({
-    url:'/core/list_ingredients_simple.php',
-    type:'GET',
-    datatype:'json',
-    success:function(data) {
-        $.each(data.data, function(key, ing) {
-   			 ingredientsLit.append($('<option ing-id="'+ing.id+'" ing-type="'+ing.type+'" data-subtext="'+ing.IUPAC+'"></option>').val(ing.name).html(ing.name + ' ('+ing.cas+')'));
-  		})
- 		ingredientsLit.selectpicker('refresh');
-    }
-});
-
-//UPDATE PURITY
-$('#ingredient').on('change', function(){
-	var ingType = $("#ingredient").find('option:selected').attr('ing-type');
-	var ingID = $("#ingredient").find('option:selected').attr('ing-id');
-
-	$.ajax({ 
-		url: 'pages/getIngInfo.php', 
-		type: 'GET',
-		data: {
-			filter: "purity",
-			id: ingID
-			},
-		dataType: 'html',
-		success: function (data) {
-		  if(ingType == 'Solvent'){
-			$("#concentration").prop("disabled", true); 
-			$("#dilutant").prop("disabled", true);
-			$('#concentration').val(100);
-            $('#dilutant').val('None');
-		  }else{
-            $("#concentration").prop("disabled", false);
-			$("#dilutant").prop("disabled", false); 
-			$('#concentration').val(data);
-		  }
-		 $("#quantity").prop("disabled", false);
-         $("#quantity").val();
-		}
-	  });
-	
-	$.ajax({ 
-		url: 'pages/getIngInfo.php', 
-		type: 'GET',
-		data: {
-			filter: "solvent",
-			id: ingID
-			},
-		dataType: 'html',
-		success: function (data) {
-		  $('#dilutant').val(data);
-		}
-	});
-
-});
 
 //Add ingredient
 $('#add_ing').on('click', '[id*=add-btn]', function () {
