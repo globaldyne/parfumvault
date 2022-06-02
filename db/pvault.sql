afda1ab6dab3d480366bc5656f96885984739724
JB <john@globaldyne.co.uk>
2022-06-02 11:53:45 +0100

    Making sure pv_online is disabled by default

--- db/pvault.sql
+++ db/pvault.sql
@@ -333,11 +333,7 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci ROW_FORMAT=COMPACT;
 
 CREATE TABLE `pv_online` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `email` varchar(255) COLLATE utf8_general_ci NOT NULL,
- `password` varchar(255) COLLATE utf8_general_ci NOT NULL,
- `enabled` INT NOT NULL DEFAULT '0', 
- PRIMARY KEY (`id`)
+ `enabled` INT NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 CREATE TABLE `IFRACategories` (
