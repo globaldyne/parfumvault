b67d990ae89c90873d3d4136ced20c1039f78e26
JB <john@globaldyne.co.uk>
2023-01-31 20:38:25 +0000

    Update inventory worth

--- core/stats_inv_data.php
+++ core/stats_inv_data.php
@@ -6,17 +6,18 @@ require_once(__ROOT__.'/inc/config.php');
 require_once(__ROOT__.'/inc/opendb.php');
 require_once(__ROOT__.'/inc/settings.php');
 
-$q = mysqli_query($conn, "SELECT SUM(price) AS total_worth FROM suppliers");
+$q = mysqli_query($conn, "SELECT price,stock,size FROM suppliers WHERE stock<>''");
 while($res = mysqli_fetch_array($q)){
     $data[] = $res;
 }
 
 foreach ($data as $d) { 
-	$r['ingredients']['total_worth'] = (string)number_format($d['total_worth'],2);
-	$r['currency'] = (string)$settings['currency'];
-
-	$response['data'][] = $r;
+	$w[] = $d['price']*$d['stock']/$d['size'];
 }
+
+$r['ingredients']['total_worth'] = number_format(array_sum($w),2);
+$r['currency'] = (string)$settings['currency'];
+$response['data'][] = $r;
 
 if(empty($r)){
 	$response['data'] = [];
