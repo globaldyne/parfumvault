e8f5312d651afd83f33209167eae9ae33052d2f7
JB <john@globaldyne.co.uk>
2023-02-01 17:51:05 +0000

    Statistics page restructure

---
+++ pages/views/stats/ifra.php
@@ -0,0 +1,31 @@
+<?php
+define('__ROOT__', dirname(dirname(dirname(dirname(__FILE__))))); 
+
+require_once(__ROOT__.'/inc/sec.php');
+require_once(__ROOT__.'/inc/config.php');
+require_once(__ROOT__.'/inc/opendb.php');
+require_once(__ROOT__.'/func/getIFRAtypes.php');
+
+if(!mysqli_num_rows(mysqli_query($conn, "SELECT id FROM IFRALibrary"))){
+	echo '<div class="alert alert-info alert-dismissible"><strong>INFO: </strong> You need to <a href="index.php?do=IFRA">import</a> the IFRA xls first.</div>';
+	return;
+}
+?>
+<script>
+var chart = AmCharts.makeChart("chartIFRA",{
+  "type"    : "pie",
+  "titleField"  : "type",
+  "valueField"  : "value",
+  "dataProvider"  : [
+<?php
+$ifratypes = mysqli_query($conn, "SELECT DISTINCT type FROM IFRALibrary");
+while($types =  mysqli_fetch_array($ifratypes)){
+?>    {
+      "type": "<?php echo $types['type'];?>",
+      "value": "<?php getIFRAtypes($types['type'],$conn);?>"
+    },
+<?php } ?>
+  ],
+});
+</script>
+<div id="chartIFRA"></div>
---
+++ pages/views/stats/ingUsage.php
@@ -0,0 +1,66 @@
+<?php
+define('__ROOT__', dirname(dirname(dirname(dirname(__FILE__))))); 
+
+require_once(__ROOT__.'/inc/sec.php');
+require_once(__ROOT__.'/inc/config.php');
+require_once(__ROOT__.'/inc/opendb.php');
+require_once(__ROOT__.'/func/getIngUsage.php');
+
+
+if(!mysqli_num_rows(mysqli_query($conn, "SELECT id FROM formulas"))){
+	echo '<div class="alert alert-info alert-dismissible"><strong>INFO: </strong> to generate statistics, add at least one formula first.</div>';
+	return;
+}
+?>
+<script>
+var chart = AmCharts.makeChart( "chartIngUsage", {
+  "type": "serial",
+  "theme": "none",
+  "dataProvider": [ 
+<?php
+$ing = mysqli_query($conn, "SELECT DISTINCT ingredient AS name FROM formulas ORDER BY ingredient ASC");
+while($allIng =  mysqli_fetch_array($ing)){
+?>
+{
+    "ingredient": "<?php echo $allIng['name'];?>",
+    "usage": "<?php getIngUsage($allIng['name'],$conn); ?>"
+}, 
+<?php } ?>				   
+
+  ],
+  "valueAxes": [{
+    "gridColor": "#FFFFFF",
+    "axisAlpha": 0,
+    "position": "left",
+    "title": "Ingredient usage"
+
+  }],
+  "gridAboveGraphs": true,
+  "startDuration": 1,
+  "graphs": [{
+    "balloonText": "[[category]]: <b>[[value]]</b>",
+    "fillAlphas": 0.8,
+    "lineAlpha": 0.2,
+    "type": "column",
+    "valueField": "usage"
+  }],
+  "chartCursor": {
+    "categoryBalloonEnabled": false,
+    "cursorAlpha": 0,
+    "zoomable": true
+  },
+  "categoryField": "ingredient",
+  "categoryAxis": {
+    "gridPosition": "start",
+    "gridAlpha": 0,
+    "tickPosition": "start",
+    "tickLength": 20,
+	"labelRotation": 45
+  },
+  "export": {
+    "enabled": false
+  }
+
+});
+</script>
+<div id="chartIngUsage"></div>
