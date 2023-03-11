c77b40d6a3ffd985f99cf7fd5951ce9966c5b14f
JB <john@globaldyne.co.uk>
2023-03-11 22:55:08 +0000

    Various improvements

--- func/getCatByID.php
+++ func/getCatByID.php
@@ -4,13 +4,10 @@ if (!defined('pvault_panel')){ die('Not Found');}
 function getCatByID($id, $image = FALSE, $conn){
 	
 	$cat = mysqli_fetch_array(mysqli_query($conn, "SELECT name,image,notes FROM ingCategory WHERE id = '$id'"));
-	if($cat['notes']){
-		$title = $cat['notes'];
-	}else{
-		$title = $cat['name'];
-	}
 	
+	$title = $cat['notes'] ?: $cat['name'];
 	$image = $cat['image']?:'/img/uknown_generic.png';
+	
 	$result = '<i rel="tip" title="'.$title.'"><img class="img_ing ing_ico_list" src="'.$image.'" /></i>';
 
 	return $result;
