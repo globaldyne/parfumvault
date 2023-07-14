f203acbeffbc2754352c19df5786551b55f58a20
JB <john@globaldyne.co.uk>
2023-07-14 23:39:13 +0100

    - Rewrite add formula modal

--- pages/listFormulas.php
+++ pages/listFormulas.php
@@ -599,10 +599,7 @@ $("#getFormMeta").on("show.bs.modal", function(e) {
   <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title">Add a new formula</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
+        <h5 class="modal-title">Add new formula</h5>
       </div>
       <div class="modal-body">
       <div id="addFormulaMsg"></div>
