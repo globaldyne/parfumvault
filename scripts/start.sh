1256795f2a185f94958f81de26557a2c3ad5dc7c
JB <john@globaldyne.co.uk>
2023-07-19 20:43:43 +0100

    - Dockerfile update

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,9 +6,6 @@ fi
 
 echo "Setting enviroment"
 touch /config/.DOCKER
-mkdir -p /tmp/php-fpm
-mkdir -p /tmp/log/php-fpm
-mkdir -p /tmp/lib/php-fpm
 
 if [ ! -f "/config/config.php" ]; then
 	cp /html/inc/config.example.php /config/config.php
@@ -17,10 +14,7 @@ else
 	ln -s /config/config.php /html/inc/config.php
 fi
 
-echo "Starting web server"
-/usr/sbin/php-fpm
-/usr/sbin/httpd -k start
 echo "----------------------------------"
 echo "READY - Perfumer's Vault Ver $(cat /html/VERSION.md)"
-touch /tmp/log/php-fpm/www-error.log
-tail -f /tmp/log/php-fpm/www-error.log
+echo "Starting web server"
+/usr/bin/php -S 0.0.0.0:8000 /html
