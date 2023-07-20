74832a7de4420dcb9332621665d0a3661ed6cc5d
JB <john@globaldyne.co.uk>
2023-07-20 16:24:59 +0100

    - Dockerfile update - Nginx conf added - php-fpm added

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-echo "Setting enviroment"
-touch /config/.DOCKER
-
 echo "----------------------------------"
 echo "READY - Perfumer's Vault Ver $(cat /html/VERSION.md)"
 echo "Starting web server"
-/usr/bin/php -S 0.0.0.0:8000 /html
+php-fpm
+nginx -e /tmp/error.log
+tail -f /tmp/*.log
