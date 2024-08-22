b046946ff65170f2244f11ceb10ca2d43f8ae39f
JB <john@globaldyne.co.uk>
2024-08-22 14:33:36 +0100

    [db03554] - Main page updates

--- index.php
+++ index.php
@@ -162,188 +162,209 @@ function chkUpdate() {
 </script>
 </head>
 <body id="page-top">
-  <div id="wrapper">
+  <div id="wrapper" class="d-flex">
     <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark">
       <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/index.php">
-        <div class="sidebar-brand-icon mt-4 mb-4">            
-        	<div class="pvLogo"><img src="/img/logo.png"></div>
+        <div class="sidebar-brand-icon mt-4 mb-4">
+          <div class="pvLogo">
+            <img src="/img/logo.png" alt="Logo">
+          </div>
         </div>
-      </a>        
+      </a>
+      
       <!-- Divider -->
       <hr class="sidebar-divider my-2">
+
       <li class="nav-item">
         <a class="nav-link" href="/?do=dashboard">
           <i class="fas fa-fw fa-tachometer-alt"></i>
-          <span>Dashboard</span></a>
+          <span>Dashboard</span>
+        </a>
       </li>
+
       <li class="nav-item">
-            <?php 
-            $formulaPages = ['listFormulas', 'genFinishedProduct', 'compareFormulas', 'Formula', 'sellFormula', 'scheduledFormulas', 'batches'];
-            $isActiveFormula = in_array($_GET['do'], $formulaPages);
-            
-            $expand_f = $isActiveFormula ? 'show' : ''; 
-            $class_f = $isActiveFormula ? '' : 'collapsed'; 
-            $aria_f = $isActiveFormula ? 'true' : 'false';
-            ?> 
-            <a class="nav-link <?php echo $class_f; ?>" href="#" data-bs-toggle="collapse" data-bs-target="#collapseFormulas" aria-expanded="<?php echo $aria_f; ?>" aria-controls="collapseFormulas">
-                <i class="fas fa-fw fa-flask"></i>
-                <span>Formula Management</span>
+        <?php 
+          $formulaPages = ['listFormulas', 'genFinishedProduct', 'compareFormulas', 'Formula', 'sellFormula', 'scheduledFormulas', 'batches'];
+          $isActiveFormula = in_array($_GET['do'], $formulaPages);
+          
+          $expand_f = $isActiveFormula ? 'show' : ''; 
+          $class_f = $isActiveFormula ? '' : 'collapsed'; 
+          $aria_f = $isActiveFormula ? 'true' : 'false';
+        ?> 
+        <a class="nav-link <?php echo $class_f; ?>" href="#" data-bs-toggle="collapse" data-bs-target="#collapseFormulas" aria-expanded="<?php echo $aria_f; ?>" aria-controls="collapseFormulas">
+          <i class="fas fa-fw fa-flask"></i>
+          <span>Formula Management</span>
+        </a>
+        <div id="collapseFormulas" class="collapse <?php echo $expand_f; ?>">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <a class="collapse-item <?php echo $_GET['do'] === 'listFormulas' || $_GET['do'] === 'Formula' ? 'active' : ''; ?>" href="/?do=listFormulas">Formulas</a>
+            <a class="collapse-item <?php echo $_GET['do'] === 'compareFormulas' ? 'active' : ''; ?>" href="/?do=compareFormulas">Compare Formulas</a>
+            <a class="collapse-item <?php echo $_GET['do'] === 'genFinishedProduct' ? 'active' : ''; ?>" href="/?do=genFinishedProduct">Finished Product</a>
+            <a class="collapse-item <?php echo $_GET['do'] === 'sellFormula' ? 'active' : ''; ?>" href="/?do=sellFormula">Sell Formula</a>
+            <a class="collapse-item <?php echo $_GET['do'] === 'scheduledFormulas' ? 'active' : ''; ?>" href="/?do=scheduledFormulas">Scheduled Formulas 
+              <span class="badge badge-danger badge-counter"><?php echo countPending(NULL, NULL, $conn); ?></span>
             </a>
-            <div id="collapseFormulas" class="collapse <?php echo $expand_f; ?>">
-                <div class="bg-white py-2 collapse-inner rounded">
-                    <a class="collapse-item <?php echo $_GET['do'] === 'listFormulas' || $_GET['do'] === 'Formula' ? 'active' : ''; ?>" href="/?do=listFormulas">Formulas</a>
-                    <a class="collapse-item <?php echo $_GET['do'] === 'compareFormulas' ? 'active' : ''; ?>" href="/?do=compareFormulas">Compare Formulas</a>
-                    <a class="collapse-item <?php echo $_GET['do'] === 'genFinishedProduct' ? 'active' : ''; ?>" href="/?do=genFinishedProduct">Finished Product</a>
-                    <a class="collapse-item <?php echo $_GET['do'] === 'sellFormula' ? 'active' : ''; ?>" href="/?do=sellFormula">Sell Formula</a>
-                    <a class="collapse-item <?php echo $_GET['do'] === 'scheduledFormulas' ? 'active' : ''; ?>" href="/?do=scheduledFormulas">Scheduled Formulas <span class="badge badge-danger badge-counter"><?php echo countPending(NULL, NULL, $conn); ?></span></a>
-                    <a class="collapse-item <?php echo $_GET['do'] === 'batches' ? 'active' : ''; ?>" href="/?do=batches">Batch history</a>
-                </div>
-            </div>
-        </li>
-        <li class="nav-item">
-            <?php 
-            $inventoryPages = ['ingredients', 'bottles', 'lids', 'suppliers', 'customers', 'compounds'];
-            $isActiveInventory = in_array($_GET['do'], $inventoryPages);
-            $expand = $isActiveInventory ? 'show' : ''; 
-            $class = $isActiveInventory ? '' : 'collapsed'; 
-            $aria = $isActiveInventory ? 'true' : 'false';
-            ?> 
-            <a class="nav-link <?php echo $class; ?>" href="#" data-bs-toggle="collapse" data-bs-target="#collapseInventory" aria-expanded="<?php echo $aria; ?>" aria-controls="collapseInventory">
-                <i class="fas fa-fw fa-warehouse"></i>
-                <span>Inventory</span>
-            </a>
-            <div id="collapseInventory" class="collapse <?php echo $expand; ?>">
-                <div class="bg-white py-2 collapse-inner rounded">
-                    <a class="collapse-item <?php echo $_GET['do'] === 'ingredients' ? 'active' : ''; ?>" href="/?do=ingredients">Ingredients</a>
-                    <a class="collapse-item <?php echo $_GET['do'] === 'suppliers' ? 'active' : ''; ?>" href="/?do=suppliers">Suppliers</a>
-                    <a class="collapse-item <?php echo $_GET['do'] === 'customers' ? 'active' : ''; ?>" href="/?do=customers">Customers</a>
-                    <a class="collapse-item <?php echo $_GET['do'] === 'compounds' ? 'active' : ''; ?>" href="/?do=compounds">Compounds</a>
-                    <a class="collapse-item <?php echo $_GET['do'] === 'bottles' ? 'active' : ''; ?>" href="/?do=bottles">Bottles</a>
-                    <a class="collapse-item <?php echo $_GET['do'] === 'lids' ? 'active' : ''; ?>" href="/?do=lids">Bottle Lids</a>
-                </div>
-            </div>
-        </li>    
-        <li class="nav-item">
+            <a class="collapse-item <?php echo $_GET['do'] === 'batches' ? 'active' : ''; ?>" href="/?do=batches">Batch history</a>
+          </div>
+        </div>
+      </li>
+
+      <li class="nav-item">
+        <?php 
+          $inventoryPages = ['ingredients', 'bottles', 'lids', 'suppliers', 'customers', 'compounds'];
+          $isActiveInventory = in_array($_GET['do'], $inventoryPages);
+          $expand = $isActiveInventory ? 'show' : ''; 
+          $class = $isActiveInventory ? '' : 'collapsed'; 
+          $aria = $isActiveInventory ? 'true' : 'false';
+        ?> 
+        <a class="nav-link <?php echo $class; ?>" href="#" data-bs-toggle="collapse" data-bs-target="#collapseInventory" aria-expanded="<?php echo $aria; ?>" aria-controls="collapseInventory">
+          <i class="fas fa-fw fa-warehouse"></i>
+          <span>Inventory</span>
+        </a>
+        <div id="collapseInventory" class="collapse <?php echo $expand; ?>">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <a class="collapse-item <?php echo $_GET['do'] === 'ingredients' ? 'active' : ''; ?>" href="/?do=ingredients">Ingredients</a>
+            <a class="collapse-item <?php echo $_GET['do'] === 'suppliers' ? 'active' : ''; ?>" href="/?do=suppliers">Suppliers</a>
+            <a class="collapse-item <?php echo $_GET['do'] === 'customers' ? 'active' : ''; ?>" href="/?do=customers">Customers</a>
+            <a class="collapse-item <?php echo $_GET['do'] === 'compounds' ? 'active' : ''; ?>" href="/?do=compounds">Compounds</a>
+            <a class="collapse-item <?php echo $_GET['do'] === 'bottles' ? 'active' : ''; ?>" href="/?do=bottles">Bottles</a>
+            <a class="collapse-item <?php echo $_GET['do'] === 'lids' ? 'active' : ''; ?>" href="/?do=lids">Bottle Lids</a>
+          </div>
+        </div>
+      </li>
+
+      <li class="nav-item">
         <a class="nav-link" href="/?do=IFRA">
           <i class="fas fa-fw fa-university"></i>
-          <span>IFRA Library</span></a>
-        </li>
-        <li class="nav-item">
+          <span>IFRA Library</span>
+        </a>
+      </li>
+      <li class="nav-item">
         <a class="nav-link" href="/?do=statistics">
           <i class="fas fa-fw fa-chart-area"></i>
-          <span>Statistics</span></a>
-        </li>
-      	<hr class="sidebar-divider d-none d-md-block">
-        <li class="nav-item">
+          <span>Statistics</span>
+        </a>
+      </li>
+
+      <hr class="sidebar-divider d-none d-md-block">
+
+      <li class="nav-item">
         <a class="nav-link" href="/?do=marketplace">
           <i class="fas fa-fw fa-store"></i>
-          <span>Marketplace</span></a>
-       </li>
-       <hr class="sidebar-divider d-none d-md-block">
-        <li class="nav-item">
+          <span>Marketplace</span>
+        </a>
+      </li>
+
+      <hr class="sidebar-divider d-none d-md-block">
+
+      <li class="nav-item">
         <a class="nav-link" href="/?do=genSDS">
           <i class="fas fa-square-poll-horizontal"></i>
           My SDSs
-          <span class="mr-5 mt-2 position-absolute top-0 translate-middle badge badge-danger badge-counter">BETA</span>
-          </a>
-       </li>
+          <span class="position-absolute top-0 translate-middle badge badge-danger badge-counter">BETA</span>
+        </a>
+      </li>
+
       <hr class="sidebar-divider d-none d-md-block">
     </ul>
+
     <?php
-		if($_GET['do'] == 'Formula'){
-			require_once(__ROOT__.'/pages/formula.php');
-		}elseif($_GET['do'] == 'ingredients'){
-			require_once(__ROOT__.'/pages/ingredients.php');
-		}elseif($_GET['do'] == 'settings'){
-			require_once(__ROOT__.'/pages/settings.php');
-		}elseif($_GET['do'] == 'marketplace'){
-			require_once(__ROOT__.'/pages/views/pvOnline/marketPlace.php');
-		}elseif($_GET['do'] == 'statistics'){
-			require_once(__ROOT__.'/pages/statistics.php');
-		}elseif($_GET['do'] == 'IFRA'){
-			require_once(__ROOT__.'/pages/IFRA.php');
-		}elseif($_GET['do'] == 'listFormulas'){
-		?>
+      if ($_GET['do'] == 'Formula') {
+          require_once(__ROOT__.'/pages/formula.php');
+      } elseif ($_GET['do'] == 'ingredients') {
+          require_once(__ROOT__.'/pages/ingredients.php');
+      } elseif ($_GET['do'] == 'settings') {
+          require_once(__ROOT__.'/pages/settings.php');
+      } elseif ($_GET['do'] == 'marketplace') {
+          require_once(__ROOT__.'/pages/views/pvOnline/marketPlace.php');
+      } elseif ($_GET['do'] == 'statistics') {
+          require_once(__ROOT__.'/pages/statistics.php');
+      } elseif ($_GET['do'] == 'IFRA') {
+          require_once(__ROOT__.'/pages/IFRA.php');
+      } elseif ($_GET['do'] == 'listFormulas') {
+    ?>
         <div id="content-wrapper" class="d-flex flex-column">
-			<?php require_once(__ROOT__.'/pages/top.php'); ?>
-        <div class="container-fluid">
-          <div>
-          <div class="card shadow mb-4">
-           
-            <div id="list_formulas">
-            	<div class="loader-center">
-                	<div class="loader"></div>
+          <?php require_once(__ROOT__.'/pages/top.php'); ?>
+          <div class="container-fluid">
+            <div>
+              <div class="card shadow mb-4">
+                <div id="list_formulas">
+                  <div class="loader-center">
+                    <div class="loader"></div>
                     <div class="loader-text"></div>
+                  </div>
                 </div>
-             </div>
-           </div>
+              </div>
+            </div>
           </div>
         </div>
-	   </div>
-		<?php
-		}elseif($_GET['do'] == 'genFinishedProduct'){
-			require_once(__ROOT__.'/pages/genFinishedProduct.php');		
-		}elseif($_GET['do'] == 'bottles'){
-			require_once(__ROOT__.'/pages/bottles.php');		
-		}elseif($_GET['do'] == 'addBottle'){
-			require_once(__ROOT__.'/pages/addBottle.php');		
-		}elseif($_GET['do'] == 'lids'){
-			require_once(__ROOT__.'/pages/lids.php');
-		}elseif($_GET['do'] == 'addLid'){
-			require_once(__ROOT__.'/pages/addLid.php');	
-		}elseif($_GET['do'] == 'batches'){
-			require_once(__ROOT__.'/pages/views/formula/batches.php');
-		}elseif($_GET['do'] == 'scheduledFormulas'){
-			require_once(__ROOT__.'/pages/scheduledFormulas.php');	
-		}elseif($_GET['do'] == 'cart'){
-			require_once(__ROOT__.'/pages/cart.php');	
-		}elseif($_GET['do'] == 'suppliers'){
-			require_once(__ROOT__.'/pages/suppliers.php');
-		}elseif($_GET['do'] == 'sellFormula'){
-			require_once(__ROOT__.'/pages/sellFormula.php');
-		}elseif($_GET['do'] == 'customers'){
-			require_once(__ROOT__.'/pages/customers.php');
-		}elseif($_GET['do'] == 'compareFormulas'){
-			require_once(__ROOT__.'/pages/compareFormulas.php');
-		}elseif($_GET['do'] == 'compounds'){
-			require_once(__ROOT__.'/pages/compounds.php');
-		}elseif($_GET['do'] == 'genSDS'){
-			require_once(__ROOT__.'/pages/views/regulatory/listSDS.php');				
-		}else{
-			require_once(__ROOT__.'/pages/dashboard.php');
-		}
-	?>
-<?php require_once(__ROOT__.'/pages/footer.php'); ?>
+    <?php
+      } elseif ($_GET['do'] == 'genFinishedProduct') {
+          require_once(__ROOT__.'/pages/genFinishedProduct.php');        
+      } elseif ($_GET['do'] == 'bottles') {
+          require_once(__ROOT__.'/pages/bottles.php');        
+      } elseif ($_GET['do'] == 'addBottle') {
+          require_once(__ROOT__.'/pages/addBottle.php');        
+      } elseif ($_GET['do'] == 'lids') {
+          require_once(__ROOT__.'/pages/lids.php');
+      } elseif ($_GET['do'] == 'addLid') {
+          require_once(__ROOT__.'/pages/addLid.php');    
+      } elseif ($_GET['do'] == 'batches') {
+          require_once(__ROOT__.'/pages/views/formula/batches.php');
+      } elseif ($_GET['do'] == 'scheduledFormulas') {
+          require_once(__ROOT__.'/pages/scheduledFormulas.php');    
+      } elseif ($_GET['do'] == 'cart') {
+          require_once(__ROOT__.'/pages/cart.php');    
+      } elseif ($_GET['do'] == 'suppliers') {
+          require_once(__ROOT__.'/pages/suppliers.php');
+      } elseif ($_GET['do'] == 'sellFormula') {
+          require_once(__ROOT__.'/pages/sellFormula.php');
+      } elseif ($_GET['do'] == 'customers') {
+          require_once(__ROOT__.'/pages/customers.php');
+      } elseif ($_GET['do'] == 'compareFormulas') {
+          require_once(__ROOT__.'/pages/compareFormulas.php');
+      } elseif ($_GET['do'] == 'compounds') {
+          require_once(__ROOT__.'/pages/compounds.php');
+      } elseif ($_GET['do'] == 'genSDS') {
+          require_once(__ROOT__.'/pages/views/regulatory/listSDS.php');                
+      } else {
+          require_once(__ROOT__.'/pages/dashboard.php');
+      }
+    ?>
+    
+    <?php require_once(__ROOT__.'/pages/footer.php'); ?>
 
-<!-- TOAST -->
-<div class="position-fixed top-0 start-50 translate-middle-x p-3" style="z-index: 11">
-  <div id="liveToast" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true" data-bs-autohide="true">
-    <div class="toast-header">
-      <strong class="me-auto" id="toast-title">...</strong>
-      <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-    </div>
-  </div>
-</div>
-
-<!--RELEASE NOTES-->
-<div class="modal fade" id="release_notes" tabindex="-1" role="dialog" aria-labelledby="release_notes" aria-hidden="true">
-  <div class="modal-dialog modal-lg" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="release_notes">Release Notes</h5>
-        <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
+    <!-- TOAST -->
+    <div class="position-fixed top-0 start-50 translate-middle-x p-3" style="z-index: 11">
+      <div id="liveToast" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true" data-bs-autohide="true">
+        <div class="toast-header">
+          <strong class="me-auto" id="toast-title">...</strong>
+          <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
       </div>
-      <div class="modal-body">
-		Thanks for updating to version <strong><?php echo $ver;?></strong> 
-	    <p><pre><?php echo file_get_contents('releasenotes.md','r');?></pre></p>
-	    <div class="modal-footer">
-	     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-	   </div>
+    </div>
+
+    <!-- RELEASE NOTES -->
+    <div class="modal fade" id="release_notes" tabindex="-1" role="dialog" aria-labelledby="release_notes" aria-hidden="true">
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="release_notes">Release Notes</h5>
+            <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            Thanks for updating to version <strong><?php echo $ver;?></strong>
+            <p><pre><?php echo file_get_contents('releasenotes.md','r');?></pre></p>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
-</div>
-
 </body>
+
 </html>
