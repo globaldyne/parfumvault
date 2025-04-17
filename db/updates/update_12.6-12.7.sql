ab7bf616fa0a2f93bc0db8d174ad2995a36176ce
JB <john@globaldyne.co.uk>
2025-04-17 15:09:08 +0100

    [ca95858a] - Backwards compatibility db schema fix

--- db/updates/update_12.6-12.7.sql
+++ db/updates/update_12.6-12.7.sql
@@ -2,5 +2,3 @@ ALTER TABLE `ingredients` ADD `aromaTrackID` VARCHAR(255) NULL DEFAULT NULL AFTE
 ALTER TABLE `ingSuppliers` ADD `currency` VARCHAR(255) NULL DEFAULT NULL AFTER `country`; 
 INSERT INTO `system_settings` (`key_name`, `value`, `slug`, `type`, `description`, `created_at`, `updated_at`) VALUES ('API_enabled', '0', 'API access', 'checkbox', 'Enable or disable API access globally', current_timestamp(), current_timestamp());
 UPDATE `ingTypes` SET `name` = 'Other/Unknown' WHERE `ingTypes`.`name` = 'Other/Uknown';
-
-ALTER TABLE `batchIDHistory` CHANGE `created` `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP;
