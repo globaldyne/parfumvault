c808a74b0b8bf091d736f9f74438959299c6becc
JB <john@globaldyne.co.uk>
2025-05-04 14:04:19 +0100

    [72fd7a0d] - Mimor UI update

--- pages/top.php
+++ pages/top.php
@@ -67,7 +67,7 @@ if (!empty($system_settings['announcements']) && !empty($user_settings)) {
 
 <!-- Announcement Modal -->
 <div class="modal fade" id="announcementModal" tabindex="-1" aria-labelledby="announcementModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
+  <div class="modal-dialog modal-lg modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="announcementModalLabel">Announcement</h5>
