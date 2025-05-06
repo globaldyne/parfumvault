4e3cd9fc89fcd6fe13da2f42743f375317cf2f78
JB <john@globaldyne.co.uk>
2025-05-06 18:32:45 +0100

    [a49cbd3d] - Mimor UI update

--- pages/listFormulas.php
+++ pages/listFormulas.php
@@ -721,7 +721,7 @@ $(document).ready(function() {
 
 <!--GET FORMULA SETTINGS MODAL-->            
 <div class="modal fade" id="getFormMeta" data-bs-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="getFormMetalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg" role="document">
+  <div class="modal-dialog modal-xl" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title mgmIngHeader mgmIngHeader-with-separator" id="getFormMetaLabel">Formula settings</h5>
