55cf6248f91c59e1b58e02f9e5228cb684872796
JB <john@globaldyne.co.uk>
2025-05-30 08:55:38 +0100

    [8d9cfed1] - Fix incorrect reload trigger for make formula page

--- pages/makeFormula.php
+++ pages/makeFormula.php
@@ -321,44 +321,70 @@ $(document).ready(function() {
 	};
 	**/
 	function reload_data() {
-		var table = $('#tdDataPending').DataTable();
-		$.ajax({
-			url: '/core/pending_formulas_data.php?meta=0&fid=' + fid,
-			type: 'GET',
-			dataType: 'json',
-			success: function(data) {
-				var localData = table.ajax.json().data;
-				var reloadNeeded = false;
-				var changeLog = [];
-				for (var i = 0; i < Math.min(localData.length, data.data.length); i++) {
-					if (localData[i].ingredient !== data.data[i].ingredient) {
-						reloadNeeded = true;
-						changeLog.push('Ingredient changed: ' + localData[i].ingredient + ' to ' + data.data[i].ingredient);
-					}
-					if (localData[i].quantity !== data.data[i].quantity) {
-						reloadNeeded = true;
-						changeLog.push('Quantity changed for ' + localData[i].ingredient + ': ' + localData[i].quantity + ' to ' + data.data[i].quantity);
-					}
-					if (localData[i].toAdd !== data.data[i].toAdd) {
-						reloadNeeded = true;
-						changeLog.push('toAdd changed for ' + localData[i].ingredient + ': ' + localData[i].toAdd + ' to ' + data.data[i].toAdd);
-					}
-					if (localData[i].toSkip !== data.data[i].toSkip) {
-						reloadNeeded = true;
-						changeLog.push('toSkip changed for ' + localData[i].ingredient + ': ' + localData[i].toSkip + ' to ' + data.data[i].toSkip);
-					}
-				}
-				if (reloadNeeded) {
-					table.ajax.reload(null, true);
-					console.log('Changes detected, data reloaded');
-				//	console.log('Change log:', changeLog);
-				}
-			},
-			error: function(xhr, status, error) {
-				console.error('Error checking data:', error);
-			}
-		});
-	};
+    var table = $('#tdDataPending').DataTable();
+    $.ajax({
+        url: '/core/pending_formulas_data.php?meta=0&fid=' + fid + '&full=1',
+        type: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            var localData = table.ajax.json().data;
+            var remoteData = data.data;
+
+            // Build maps by id for fast lookup
+            var localMap = {};
+            localData.forEach(function(item) { localMap[item.id] = item; });
+            var remoteMap = {};
+            remoteData.forEach(function(item) { remoteMap[item.id] = item; });
+
+            var reloadNeeded = false;
+            var changeLog = [];
+
+            // Check for changes and missing/extra items
+            Object.keys(remoteMap).forEach(function(id) {
+                var local = localMap[id];
+                var remote = remoteMap[id];
+                if (!local) {
+                    reloadNeeded = true;
+                    changeLog.push('New ingredient: ' + remote.ingredient);
+                    return;
+                }
+                if (local.ingredient !== remote.ingredient) {
+                    reloadNeeded = true;
+                    changeLog.push('Ingredient changed: ' + local.ingredient + ' to ' + remote.ingredient);
+                }
+                if (local.quantity !== remote.quantity) {
+                    reloadNeeded = true;
+                    changeLog.push('Quantity changed for ' + local.ingredient + ': ' + local.quantity + ' to ' + remote.quantity);
+                }
+                if (local.toAdd !== remote.toAdd) {
+                    reloadNeeded = true;
+                    changeLog.push('toAdd changed for ' + local.ingredient + ': ' + local.toAdd + ' to ' + remote.toAdd);
+                }
+                if (local.toSkip !== remote.toSkip) {
+                    reloadNeeded = true;
+                    changeLog.push('toSkip changed for ' + local.ingredient + ': ' + local.toSkip + ' to ' + remote.toSkip);
+                }
+            });
+
+            // Check for removed items
+            Object.keys(localMap).forEach(function(id) {
+                if (!remoteMap[id]) {
+                    reloadNeeded = true;
+                    changeLog.push('Ingredient removed: ' + localMap[id].ingredient);
+                }
+            });
+
+            if (reloadNeeded) {
+                table.ajax.reload(null, true);
+                console.log('Changes detected, data reloaded');
+                console.log('Change log:', changeLog);
+            }
+        },
+        error: function(xhr, status, error) {
+            console.error('Error checking data:', error);
+        }
+    });
+};
 
     setInterval(reload_data, 5000); // Check for updates every 5 seconds
 
